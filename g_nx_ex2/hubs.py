21e76e63c4c763c9da4b8cd0f0f678947ca2f75d
gsec <o0v0o.ix@gmail.com>
2013-01-16 16:55:44 +0100

    excercise 2 finished and submitted

--- g_nx_ex2/hubs.py
+++ g_nx_ex2/hubs.py
@@ -1,148 +1,95 @@
-########################### network excercise 2 ###############################
-#########################--- killing a network ---#############################
-######################### Guilherme Stein (2013) ##############################
-######### in collaboration with: Florian, Ulrich & Daniel #####################
-
-
+############################ Guilherme Stein & Florian Amman (2013) ################################
+########################### in collaboration with: Ulrich & Daniel #################################
 
 import numpy as np
-from pylab import *
-from scipy import optimize  
-from scipy import stats
 import networkx as nx
 import random as rd
 import matplotlib.pyplot as plt
+import copy
 
 def generate_graph(gamma, kmin, kmax, dk):
-    rnd = np.random.random(graph_size)
-        # map probability to power law:
+    # map probability to power law:
+    p = np.random.random(graph_size)
     C = (-gamma+1) / ((kmax+dk)**(-gamma+1) - (kmin+dk)**(-gamma+1))
-    k = ( (-gamma+1)/C * rnd + (kmin+dk)**(-gamma + 1))**(1/(-gamma + 1)) - dk
-    sequence = map(int, sorted(np.round(k)))    # map sorted intervalls to int
-    if np.mod(np.sum(sequence), 2) != 0:        # if odd
-        sequence[-1]-=1                         # get rid of last element
-    G = nx.configuration_model(sequence)        # generate random graph
-    G.remove_edges_from(G.selfloop_edges())     # remove self loops
-    G = nx.Graph(G)               # generates ordinary graphs without parallel
-                                  # edges but introduces error
+    k = ((-gamma+1)/C * p + (kmin+dk)**(-gamma + 1)) ** (1/(-gamma+1)) - dk
+    sequence = map(int, sorted(np.round(k)))                    # map sorted intervalls to int
+    if np.mod(np.sum(sequence), 2) != 0:                        # if odd, get rid of last element
+        sequence[-1]-=1                                 
+    G = nx.configuration_model(sequence)                
+    G = nx.Graph(G)                                             # remove multiple connections
     return G
 
+def kill_random_node(at_once):
+    kill_list = rd.sample(range(0, G.number_of_nodes() - 1),    # generate sample of removal 
+                          at_once)                              
+    removable_nodes = G.nodes()                                 # which nodes are available
+    for i in range(len(kill_list)):                             # remove all elements specified
+        G.remove_node(removable_nodes[kill_list[i]])            # by kill_list
+
+def kill_hub(at_once):
+    for i in range(at_once):
+        max_index=np.argmax(edges_per_node)                     # returns index of largest node
+        edges_per_node[max_index]=0                             # set node as smallest in list
+        G.remove_node(max_index)                                # remove node from graph
+ 
 def plot_graph():
     nx.draw(G)
     show()
-
-def random_kill_a_node(number):
-    kill_list = rd.sample(range(0, G.number_of_nodes() - 1), number)
-    # kill_list=np.random.random_integers(0,G.number_of_nodes()-1,number)
-    rem_nodes=G.nodes() 
-    tmp=np.array([])
-    for i in range(len(kill_list)):
-        tmp=np.append(tmp,rem_nodes[kill_list[i]])
-    G.remove_nodes_from(tmp)
-
-# def hub_kill_a_node(number):
-    # kill_list=G.number_of_edges()[:number] #rd.sample(range(0,G.number_of_nodes()-1),number)
-    # rem_nodes=G.nodes()   
-    # tmp=np.array([])
-    # for i in range(len(kill_list)):
-        # tmp=np.append(tmp,rem_nodes[kill_list[i]])
-    # G.remove_nodes_from(tmp)
     
-# --- main ---  
-graph_size=10000
-gamma=2.5
-kmin=1.0
-kmax=float(graph_size-1)
-dk=0.0
-killed=0.0
-killstep=50
-y=np.array([])
-x=np.array([])
+# ------ main ------  
+graph_size = 100000                                             # initialize variables
+gamma = 3.5
+kmin = 1.0
+kmax = float(graph_size-1)
+dk = 0.0
+killstep = 50                                                   # how many are removed at once
+y1 = np.array([])
+x1 = np.array([])
+y2 = np.array([])
+x2 = np.array([])
 
-G=generate_graph(gamma,kmin,kmax,dk)
-Gcc0=len(nx.connected_components(G)[0])
-Gcc=Gcc0
-
-
-while Gcc/Gcc0 > 0.01:
-    random_kill_a_node(killstep)
-    killed+=killstep
-    Gcc=len(nx.connected_components(G)[0])*1.0
-    x=np.append(x,killed/graph_size)
-    y=np.append(y,Gcc/Gcc0)
-    print(killed/graph_size)
-# print(x)  
-# print(y)
+G0 = generate_graph(gamma,kmin,kmax,dk)                         # generates graph
+Gcc0 = len(nx.connected_components(G0)[0])                      # size of giant component
 
 
-fig=plt.figure()
-legend(loc=2)
-ax=fig.add_subplot(111)
-ax.plot(x,y,color='red',label='o')
+Gcc = copy.copy(Gcc0)                                           # make instance for Random kill
+G = G0.copy()
+killed = 0.0
+
+while Gcc/Gcc0 > 0.001 and G.number_of_nodes > killstep:
+    kill_random_node(killstep)
+    killed += killstep
+    print 'Random Kill: ', killed/graph_size                    
+    Gcc = float(len(nx.connected_components(G)[0]))
+    x1 = np.append(x1, killed/graph_size)
+    y1 = np.append(y1, Gcc/Gcc0)
+
+
+Gcc = copy.copy(Gcc0)                                           # make instance for Hub kill
+G = G0.copy()
+killed = 0.0
+
+edges_per_node = np.array(())                                   # count edges for each node
+for i in range(graph_size):
+    edges_per_node = np.append(edges_per_node, int(len(G.edges(i))))
+
+while Gcc/Gcc0 > 0.001 and G.number_of_nodes > killstep:
+    kill_hub(killstep)
+    killed += killstep
+    print 'Hub Kill: ', killed/graph_size
+    Gcc = float(len(nx.connected_components(G)[0]))
+    x2 = np.append(x2, killed/graph_size)
+    y2 = np.append(y2, Gcc/Gcc0)
+
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot(111)
+ax1.plot(x1, y1, color='red', linewidth=2, label="Kill random nodes")
+ax1.set_title('$\gamma$ = '+ str(gamma))
+
+ax2 = fig.add_subplot(111)
+ax2.plot(x2, y2, color='blue', linewidth=2, label="Kill largest hubs")
+
+plt.legend(loc='upper right')
 plt.show()
-
-
-
-
-
-#plot_graph()
-
-
-
-
-
-# N=100000
-# Punkte=200
-# kv=np.linspace(0,4,Punkte)
-# S=np.linspace(0,0,Punkte)
-# s=np.linspace(0,0,Punkte)
-
-# i=0
-# for k in kv:
-    # p=k/N
-    # G=nx.fast_gnp_random_graph(N,p)
-    # Gcc=nx.connected_components(G)
-    # S[i]=len(Gcc[0])*1.0/N
-    # s[i]=(N-len(Gcc[0]))*1.0/(len(Gcc)-1) 
-    # i=i+1
-    # print(i)
-
-# sp=1/(1-kv+kv*S)
-# logS=log(S)
-# logk=log(kv-1)
-
-# # --- fit 
-# fitfunc = lambda p, x: p[0]+p[1]*x    
-# def residuals(p, y, x):
-    # err = y-fitfunc(p,x)
-    # return err
-# p0 = [1,1]    
-# p1, success = optimize.leastsq(residuals, p0[:], args=(logS, logk))
-# print(logS,logk)
-    
-# # --- plot    
-# fig=plt.figure()
-# legend(loc=2)
-# ax=fig.add_subplot(111)
-# ax.set_ylabel(u'S')
-# ax.set_xlabel(u'<k>')
-# ax.plot(kv,S,color='blue',label='S')
-# show()
-
-# fig=plt.figure()
-# legend(loc=2)
-# ax=fig.add_subplot(111)
-# ax.set_ylabel(u'S')
-# ax.set_xlabel(u'<k>')
-# ax.plot(kv,s,color='red',label='s')
-# show()
-
-# fig=plt.figure()
-# legend(loc=2)
-# ax=fig.add_subplot(111)
-# ax.set_ylabel(u'log(S)')
-# ax.set_xlabel(u'log(<k>-1)')
-# ax.plot(logk,fitfunc(p1,logk),color='green',label='fitfunc')
-# ax.plot(logk,logS,color='blue',label='logS')
-# print(p1)
-# show()    
